d8e1df6bfbb06fafd8e798f013831303287aba60
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-12 20:43:54 +0200

    Update some components with @material-ui template

--- react-ts-material-ui-crud-demo/src/App.tsx
+++ react-ts-material-ui-crud-demo/src/App.tsx
@@ -1,14 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+//import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Button from "@material-ui/core/Button";
+
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { createStyles, alpha, Theme, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import TutorialComponent from './components/TutorialComponent';
 import AddTutorialComponent from './components/AddTutorialComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <AddTutorialComponent />
-    </div>
-  );
-}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }),
+);
 
-export default App;
+export default function App () {
+
+  const classes = useStyles();
+  //render() {
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Button
+              variant="contained"
+              color="default"
+              style={{ borderRadius: "10px" }}
+            >
+              <Link to={"/tutorials"} className="">
+                vscalcione
+              </Link>
+            </Button>
+
+            <Button
+              variant="contained"
+              color="default"
+              style={{ marginLeft: "15px", borderRadius: "10px" }}
+            >
+              <Link to={"/tutorials"} className="">
+                Tutorials
+              </Link>
+            </Button>
+
+            <Button
+              variant="contained"
+              color="default"
+              style={{ marginLeft: "15px", borderRadius: "10px" }}
+            >
+              <Link to={"/add"} className="">
+                Add
+              </Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={TutorialComponent} />
+          <Route exact path={["/add"]} component={AddTutorialComponent} />
+        </Switch>
+    </div>
+      /** 
+      <div>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialComponent} />
+          </Switch>
+        </div>
+      </div>*/
+    );
+  //}
+};
--- react-ts-material-ui-crud-demo/src/components/TutorialComponent.tsx
+++ react-ts-material-ui-crud-demo/src/components/TutorialComponent.tsx
@@ -1,5 +1,225 @@
-import React from 'react';
+import React, { Component, ChangeEvent } from "react";
+import { RouteComponentProps } from "react-router-dom";
 
-export const TutorialComponent = () => {
-    
+import TutorialDataService from "../services/tutorial.service";
+import ITutorialData from "../types/tutorial.type";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+
+interface RouterProps {
+  id: string;
+}
+
+type Props = RouteComponentProps<RouterProps>;
+type State = {
+  currentTutorial: ITutorialData;
+  message: string;
+};
+
+export default class TutorialComponent extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.getTutorial = this.getTutorial.bind(this);
+    this.updatePublished = this.updatePublished.bind(this);
+    this.updateTutorial = this.updateTutorial.bind(this);
+    this.deleteTutorial = this.deleteTutorial.bind(this);
+
+    this.state = {
+      currentTutorial: {
+        id: null,
+        title: "",
+        description: "",
+        published: false,
+      },
+      message: "",
+    };
+  }
+
+  componentDidMount = () => {
+    this.getTutorial(this.props.match.params.id);
+  };
+
+  onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    this.setState(function (prevState) {
+      return {
+        currentTutorial: {
+          ...prevState.currentTutorial,
+          title: title,
+        },
+      };
+    });
+  };
+
+  onChangeDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    const description = e.target.value;
+    this.setState((prevState) => ({
+      currentTutorial: {
+        ...prevState.currentTutorial,
+        description: description,
+      },
+    }));
+  };
+
+  getTutorial = (id: string) => {
+    TutorialDataService.get(id)
+      .then((response) => {
+        this.setState({
+          currentTutorial: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  updatePublished = (status: boolean) => {
+    const data: ITutorialData = {
+      id: this.state.currentTutorial.id,
+      title: this.state.currentTutorial.title,
+      description: this.state.currentTutorial.description,
+      published: status,
+    };
+
+    TutorialDataService.update(data, this.state.currentTutorial.id)
+      .then((response) => {
+        this.setState((prevState) => ({
+          currentTutorial: {
+            ...prevState.currentTutorial,
+            published: status,
+          },
+          message: "The status was updated successfully",
+        }));
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  updateTutorial = () => {
+    TutorialDataService.update(
+      this.state.currentTutorial,
+      this.state.currentTutorial.id
+    )
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          message: "The tutorial was updated successfully!",
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  deleteTutorial = () => {
+    TutorialDataService.delete(this.state.currentTutorial.id)
+      .then((response) => {
+        console.log(response.data);
+        this.props.history.push("/tutorials");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  render() {
+    const { currentTutorial } = this.state;
+
+    return (
+      <div>
+        {currentTutorial ? (
+          <div className="edit-form">
+            <h4>Tutorial</h4>
+            <form>
+              <div className="form-group">
+                <br />
+                <TextField
+                  name="title"
+                  id="title"
+                  label="Title"
+                  variant="outlined"
+                  required
+                  value={currentTutorial.title}
+                  onChange={this.onChangeTitle}
+                />
+              </div>
+              <div className="form-group">
+                <br />
+                <TextField
+                  name="description"
+                  id="description"
+                  label="Description"
+                  variant="outlined"
+                  required
+                  value={currentTutorial.description}
+                  onChange={this.onChangeDescription}
+                />
+              </div>
+
+              <div className="form-group">
+                <br />
+                <label>
+                  <strong>Status:</strong>
+                </label>
+                {currentTutorial.published ? "Published" : "Pending"}
+              </div>
+            </form>
+
+            {currentTutorial.published ? (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => this.updatePublished(false)}
+                className="margin-top-form"
+                style={{ marginTop: "10px", marginLeft: "10px" }}
+              >
+                UnPublish  
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => this.updatePublished(true)}
+                style={{ marginTop: "10px" }}
+              >
+                Publish  
+              </Button>
+            )}
+
+            <Button 
+              variant="contained" 
+              color="secondary"
+              onClick={this.deleteTutorial}
+              style={{ marginTop: "10px", marginLeft: "10px" }}
+            >
+              Delete
+            </Button>
+
+            <Button 
+              variant="contained"
+              onClick={this.updateTutorial}
+              style={{ marginTop: "10px", marginLeft: "10px" }}
+            >
+              Update
+            </Button>
+
+            <p>{this.state.message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Tutorial...</p>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
